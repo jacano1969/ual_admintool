7514ddbfa7e24b1b3313fbea192aa74beca08c80
Daniel Somers <d.somers@ulcc.ac.uk>
2012-11-08 08:26:10 +0000

    updated db server name

--- dbconfig.php
+++ dbconfig.php
@@ -4,7 +4,7 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->db_host='localhost:3306';
+$CFG->db_host='moodle02.arts.ulcc.ac.uk';
 $CFG->db_name='dev_ualmis';
 $CFG->db_user='spider';
 $CFG->db_pass='mwfumtrb';
