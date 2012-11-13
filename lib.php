d19a964f8200d947a2fb0ffa42744a07b72a606a
Daniel Somers <d.somers@ulcc.ac.uk>
2012-11-13 15:28:17 +0000

    testing filters

--- lib.php
+++ lib.php
@@ -381,7 +381,7 @@ function get_filter_data($type=false, $data=false) {
         }
         
         // filter by programme 
-        if($type=='P') {
+        if($type=='C') {
             // TODO:
             //$courses_sql = "select distinct c.aos_code as id, c.full_description as name from courses c inner join course_structure cs on cs.aos_code=c.aos_code and cs.aos_code REGEXP '^[0-9]' inner join enrolments e on e.studentid='$loggedin_username' and c.courseid=e.courseid inner join course_structure cs1 on cs1.aoscd_link=c.aos_code and cs1.aos_code='$data' order by name;";
             $courses_sql = "select c.aos_code as id, c.full_description as name from courses where c.aos_code='$data'";
