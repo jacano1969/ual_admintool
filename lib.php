36b8c1a6c080291a2815892162415e4610a33694
Daniel Somers <d.somers@ulcc.ac.uk>
2012-11-13 10:24:31 +0000

    updated filters

--- lib.php
+++ lib.php
@@ -277,7 +277,7 @@ function get_filter_data($type=false, $data=false) {
     //$programmes_sql = "select distinct aos_code as id, concat(aos_code, aos_period, acad_period) as name from course_structure where aos_code like('L%') order by name";
     //if($type==false) {
         //$programmes_sql = "select distinct cs.aos_code as id, full_description as name from course_structure cs inner join enrolments e on e.studentid='$loggedin_username' and e.courseid=concat(cs.aos_code, cs.aos_period, cs.acad_period) and cs.aos_code like('L%') order by name";
-        $programmes_sql = "select distinct c.aos_code as id, c.full_description as name from courses inner join enrolments e on e.studentid='$loggedin_username' and e.courseid=concat(c.aos_code, c.aos_period, c.acad_period) and c.aos_code like('L%') order by name";
+        $programmes_sql = "select distinct c.aos_code as id, c.full_description as name from courses c inner join enrolments e on e.studentid='$loggedin_username' and e.courseid=concat(c.aos_code, c.aos_period, c.acad_period) and c.aos_code like('L%') order by name";
     //}
     
     // course years
