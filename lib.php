7ae2eb8bd92f818eb5825ea61dd36a77ecb29371
Daniel Somers <d.somers@ulcc.ac.uk>
2012-11-13 14:22:53 +0000

    testing filters

--- lib.php
+++ lib.php
@@ -368,7 +368,7 @@ function get_filter_data($type=false, $data=false) {
     } else {
         // filter by programme 
         if($type=='P') {
-            $courses_sql = "select distinct c.aos_code as id, c.full_description as name from courses c inner join course_structure cs on cs.aos_code=c.aos_code and cs.aos_code REGEXP '^[0-9]' inner join enrolments e on e.studentid='$loggedin_username' inner join course_structure cs on cs.aoscd_link=c.aos_code and cs.aos_code='$data' and c.courseid=e.courseid order by name";
+            $courses_sql = "select distinct c.aos_code as id, c.full_description as name from courses c inner join course_structure cs on cs.aos_code=c.aos_code and cs.aos_code REGEXP '^[0-9]' inner join enrolments e on e.studentid='$loggedin_username' and c.courseid=e.courseid inner join course_structure cs1 on cs1.aoscd_link=c.aos_code and cs1.aos_code='$data' order by name;";
         }
     }
     
