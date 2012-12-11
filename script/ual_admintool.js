5768552340b79f88a71e1b0b0a0a804cbede4fb7
Daniel Somers <d.somers@ulcc.ac.uk>
2012-12-11 15:03:49 +0000

    chnaged font

--- script/ual_admintool.js
+++ script/ual_admintool.js
@@ -45,8 +45,8 @@ var ual_admintool = ual_admintool || (function(){
                 
                 var selected_workflow_step = $("option:selected", this).attr("id");
                 
+				// set the help text
 				var selected_workflow_help = $("option:selected", this).attr("help");
-
 				$('#helptext').html(selected_workflow_help);
 				
                 if(selected_workflow_step!="0") {
