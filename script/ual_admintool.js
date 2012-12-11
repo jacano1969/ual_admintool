ae252b9621c9194e53f32960ef679c093319d400
Daniel Somers <d.somers@ulcc.ac.uk>
2012-12-11 14:45:05 +0000

    add help text setter

--- script/ual_admintool.js
+++ script/ual_admintool.js
@@ -45,8 +45,8 @@ var ual_admintool = ual_admintool || (function(){
                 
                 var selected_workflow_step = $("option:selected", this).attr("id");
                 
-				var selected_workflow_help = $("option:selected", this).attr("help");
-				alert(selected_workflow_help);
+				var selected_workflow_help = $("#workflows option:selected", this).attr("help");
+
 				$('#helptext').html(selected_workflow_help);
 				
                 if(selected_workflow_step!="0") {
