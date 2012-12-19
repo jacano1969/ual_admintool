606a958a02487a185a4f8dc4503cc2d21e2e285a
Daniel Somers <d.somers@ulcc.ac.uk>
2012-12-19 09:34:42 +0000

    testing

--- script/ual_admintool.js
+++ script/ual_admintool.js
@@ -287,6 +287,8 @@ var ual_admintool = ual_admintool || (function(){
                         
                         jsonString += '}';
 						
+						alert(jsonString); return false;
+						
 						var action_desc = $('.container fieldset legend').text();
 						
                         // submit data 
