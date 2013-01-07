4d9bedec09cf178b59cdca13c27477823eca433e
Daniel Somers <d.somers@ulcc.ac.uk>
2013-01-07 11:43:22 +0000

    re-added filters js

--- script/ual_admintool.js
+++ script/ual_admintool.js
@@ -19,6 +19,64 @@ var ual_admintool = ual_admintool || (function(){
             
 			
 			//
+            // Filters
+            //
+            
+            // programme filter change
+            $('#programmes').live("change", function(){
+                
+                // get selected filter
+                var selected_programme = $(this).children(":selected").attr("id");
+                
+                if(selected_programme!="0") {
+                    // filter based on selected programme
+                    $.get('filter.php?type=P&data='+selected_programme, function(data){
+                        $('#mainfilters').hide();
+                        // replace filters with new data
+                        $('#mainfilters').html(data);
+                        $('#mainfilters').show();
+                    });
+                } else {
+                    // show all
+                    $.get('filter.php?type=$data=', function(data){
+                        $('#mainfilters').hide();
+                        // replace filters with new data
+                        $('#mainfilters').html(data);
+                        $('#mainfilters').show();
+                    });
+                }
+            });
+            
+            // course filter change
+            $('#courses').live("change", function(){
+                
+                // get selected filter
+                var selected_course = $(this).children(":selected").attr("id");
+                
+                if(selected_course!="0") {
+                    // filter based on selected programme
+                    $.get('filter.php?type=C&data='+selected_course, function(data){
+                        $('#mainfilters').hide();
+                        // replace filters with new data
+                        $('#mainfilters').html(data);
+                        $('#mainfilters').show();
+                    });
+                } else {
+                    // course filter has been cleared ...
+                    // get the currently selected programme
+                    var selected_programme = $('#programmes').children(":selected").attr("id");
+                    
+                    // show filters based on the selected programme
+                    $.get('filter.php?type=P$data='+selected_programme, function(data){
+                        $('#mainfilters').hide();
+                        // replace filters with new data
+                        $('#mainfilters').html(data);
+                        $('#mainfilters').show();
+                    });
+                }
+            });
+			
+			//
 			// data grid
 			//
 			
@@ -369,65 +427,6 @@ var ual_admintool = ual_admintool || (function(){
                 // show the home screen
                 window.location.href='index.php';
                 
-            });
-            
-            
-            //
-            // Filters
-            //
-            
-            // programme filter change
-            $('#programmes').live("change", function(){
-                
-                // get selected filter
-                var selected_programme = $(this).children(":selected").attr("id");
-                
-                if(selected_programme!="0") {
-                    // filter based on selected programme
-                    $.get('filter.php?type=P&data='+selected_programme, function(data){
-                        $('#mainfilters').hide();
-                        // replace filters with new data
-                        $('#mainfilters').html(data);
-                        $('#mainfilters').show();
-                    });
-                } else {
-                    // show all
-                    $.get('filter.php?type=$data=', function(data){
-                        $('#mainfilters').hide();
-                        // replace filters with new data
-                        $('#mainfilters').html(data);
-                        $('#mainfilters').show();
-                    });
-                }
-            });
-            
-            // course filter change
-            $('#courses').live("change", function(){
-                
-                // get selected filter
-                var selected_course = $(this).children(":selected").attr("id");
-                
-                if(selected_course!="0") {
-                    // filter based on selected programme
-                    $.get('filter.php?type=C&data='+selected_course, function(data){
-                        $('#mainfilters').hide();
-                        // replace filters with new data
-                        $('#mainfilters').html(data);
-                        $('#mainfilters').show();
-                    });
-                } else {
-                    // course filter has been cleared ...
-                    // get the currently selected programme
-                    var selected_programme = $('#programmes').children(":selected").attr("id");
-                    
-                    // show filters based on the selected programme
-                    $.get('filter.php?type=P$data='+selected_programme, function(data){
-                        $('#mainfilters').hide();
-                        // replace filters with new data
-                        $('#mainfilters').html(data);
-                        $('#mainfilters').show();
-                    });
-                }
             });
         }
 		
