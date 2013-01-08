1980e9b716e59a0819e8264efeb5d1dd32c4b024
Daniel Somers <d.somers@ulcc.ac.uk>
2013-01-08 08:49:23 +0000

    testing

--- results.php
+++ results.php
@@ -7,7 +7,7 @@ require_once('lib.php');
 if(is_logged_in()){
 
     // get results based on filters
-    echo 'test';
+    echo '<input type="button" class="close" value="close">';
     
 } else {
     header('Location: login.php');
