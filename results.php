e7905284cac1a020ae071900aef2d9739bf449d2
Daniel Somers <d.somers@ulcc.ac.uk>
2013-01-08 13:00:03 +0000

    added grid for enrolments

--- results.php
+++ results.php
@@ -2,7 +2,8 @@
 
 session_start();
 
-require_once('lib.php');
+require_once('dbconfig.php');
+//require_once('lib.php');
 
 $programme='';
 $course_year='';
@@ -46,13 +47,78 @@ if(is_logged_in()){
     // get results based on filters
     $content ='';
     
+    global $CFG;
+    
+    // get all enrolments for the currently logged in user
+    // based on filter
+    $mysqli =  new mysqli($CFG->db_host, $CFG->db_user, $CFG->db_pass, $CFG->db_name);
+    
+    $results ='';
+    
+    if (mysqli_connect_error()) {
+        header('Location: login.php?error=4');
+        exit;
+    }
+    $loggedin_username = $_SESSION['USERNAME'];
+    
+    $enrolments_sql = "SELECT " .
+                      "CASE WHEN c.aos_code like('L%') THEN 'Programme' ELSE " . 
+                      "CASE WHEN c.aos_code REGEXP '^[0-9]' THEN 'Course' ELSE " .
+                      "CASE WHEN c.aos_code REGEXP '^[A-Z]' THEN 'Unit' " .
+                      "END END END as 'Type', " .
+                      "e.record_id, e.enrolmentid, e.staffid, e.stageid, " .
+                      "c.courseid, c.aos_code, c.aos_period, c.acad_period, c.college, c.aos_description," .
+                      "c.full_description, c.school,c.aos_type " .
+                      "from STAFF_ENROLMENTS e " . 
+                      "inner join COURSES c on c.courseid = e.courseid " .
+                      "inner join COURSE_STRUCTURE cs on cs.aos_code = c.aos_code " .
+                      "and e.staffid = '$loggedin_username'";
+                      
+    /*if($programme!=0){
+        $enrolments_sql .= "inner join ";
+        select distinct c.aos_code as id,
+                        c.full_description as name,
+                        c.acad_period as year
+                        from COURSES c
+                        inner join STAFF_ENROLMENTS e on e.staffid='$loggedin_username'
+                        and e.courseid=concat(c.aos_code, c.aos_period, c.acad_period)
+                        and c.aos_code like('L%') order by name
+    }*/
+    
+    
     $content .='<input type="button" class="close" value="close">';
     
     $content .='<table>';
     
-    $content .='<tr><td>Programme</td><td>Course Year</td><td>Course</td><td>Unit</td></tr>';
-    $content .='<tr><td>'.$programme.'</td><td>'.$course_year.'</td><td>'.$course.'</td><td>'.$unit.'</td></tr>';
-    
+    if ($results = $mysqli->query($programmes_sql)) {
+        if($results->num_rows==0) {
+            $content .= '<tr><td>No Data</td></tr>';
+    } else {
+        $content .='<tr>';
+        
+        $content .='<td>Type</td><td>Record Id</td><td>Enrolment Id</td><td>Staff Id</td><td>Stage Id</td>';
+        $content .='<td>Course Id</td><td>AOS Code</td><td>AOS Period</td><td>ACAD Period</td>';
+        $content .='<td>College</td><td>ASO Description</td><td>Full Description</td><td>School</td><td>AOS Type</td>';
+        
+        $content .='</tr>';
+        foreach($results as $result) {
+            $content .="<tr>";
+            $content .="<td>$result->Type</td>";
+            $content .="<td>$result->record_id</td>";
+            $content .="<td>$result->staffid</td>";
+            $content .="<td>$result->stageid</td>";
+            $content .="<td>$result->courseid</td>";
+            $content .="<td>$result->aos_code</td>";
+            $content .="<td>$result->aos_period</td>";
+            $content .="<td>$result->acad_period</td>";
+            $content .="<td>$result->college</td>";
+            $content .="<td>$result->aos_description</td>";
+            $content .="<td>$result->full_description</td>";
+            $content .="<td>$result->school</td>";
+            $content .="<td>$result->aos_type</td>";
+            $content .="</tr>";
+        }
+    }   
     
     $content .='</table>';
     
