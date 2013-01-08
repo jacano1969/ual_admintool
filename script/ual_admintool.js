084d47b5122ec9a3652f00ad52e047f39d001930
Daniel Somers <d.somers@ulcc.ac.uk>
2013-01-08 08:57:01 +0000

    testing

--- script/ual_admintool.js
+++ script/ual_admintool.js
@@ -29,7 +29,7 @@ var ual_admintool = ual_admintool || (function(){
                 var selected_programme = $(this).children(":selected").attr("id");
                 
                 if(selected_programme!="0") {
-					$('#mainfilters').html('<h2>Loading ...</h2>');
+					$('#filters').html('<h2>Loading ...</h2>');
                     // filter based on selected programme
                     $.get('filter.php?type=P&data='+selected_programme, function(data){
                         $('#mainfilters').hide();
@@ -55,7 +55,7 @@ var ual_admintool = ual_admintool || (function(){
                 var selected_course = $(this).children(":selected").attr("id");
                 
                 if(selected_course!="0") {
-					$('#mainfilters').html('<h2>Loading ...</h2>');
+					$('#filters').html('<h2>Loading ...</h2>');
                     // filter based on selected programme
                     $.get('filter.php?type=C&data='+selected_course, function(data){
                         $('#mainfilters').hide();
@@ -85,7 +85,7 @@ var ual_admintool = ual_admintool || (function(){
                 var selected_year = $(this).children(":selected").attr("id");
                 
                 if(selected_year!="0") {
-					$('#mainfilters').html('<h2>Loading ...</h2>');
+					$('#filters').html('<h2>Loading ...</h2>');
                     // filter based on selected programme
                     $.get('filter.php?type=Y&data='+selected_year, function(data){
                         $('#mainfilters').hide();
@@ -100,7 +100,7 @@ var ual_admintool = ual_admintool || (function(){
                     
                     // show filters based on the selected programme
                     $.get('filter.php?type=P$data='+selected_programme, function(data){
-                        $('#mainfilters').hide();
+                        $('#filters').hide();
                         // replace filters with new data
                         $('#mainfilters').html(data);
                         $('#mainfilters').show();
@@ -115,7 +115,7 @@ var ual_admintool = ual_admintool || (function(){
                 var selected_unit = $(this).children(":selected").attr("id");
                 
                 if(selected_unit!="0") {
-					$('#mainfilters').html('<h2>Loading ...</h2>');
+					$('#filters').html('<h2>Loading ...</h2>');
                     // filter based on selected unit
                     $.get('filter.php?type=U&data='+selected_unit, function(data){
                         $('#mainfilters').hide();
