d803d15703f271cdb5e3f7bee1846aa99ad7a92a
Daniel Somers <d.somers@ulcc.ac.uk>
2013-01-17 12:36:57 +0000

    testing

--- grid.php
+++ grid.php
@@ -224,7 +224,7 @@
                 // show users enrolments
                 if($result_type=='ue' || $result_type=='') {
                     //$content .='<th class="sorting_desc" rowspan="1" colspan="1">Remove</th><th class="sorting" rowspan="1" colspan="1">Type</th><th class="sorting" rowspan="1" colspan="1">Record Id</th><th class="sorting" rowspan="1" colspan="1">Enrolment Id</th><th class="sorting" rowspan="1" colspan="1">Staff Id</th><th class="sorting" rowspan="1" colspan="1">Stage Id</th>';
-                    $content .='<th class="sorting_desc" rowspan="1" colspan="1">Type</th><th class="sorting" rowspan="1" colspan="1">Record Id</th><th class="sorting" rowspan="1" colspan="1">Enrolment Id</th><th class="sorting" rowspan="1" colspan="1">Staff Id</th><th class="sorting" rowspan="1" colspan="1">Stage Id</th>';
+                    $content .='<th class="sorting_desc" rowspan="1" colspan="1">Record Id</th><th class="sorting" rowspan="1" colspan="1">Type</th><th class="sorting" rowspan="1" colspan="1">Enrolment Id</th><th class="sorting" rowspan="1" colspan="1">Staff Id</th><th class="sorting" rowspan="1" colspan="1">Stage Id</th>';
                     $content .='<th class="sorting" rowspan="1" colspan="1">Course Id</th><th class="sorting" rowspan="1" colspan="1">AOS Code</th><th class="sorting" rowspan="1" colspan="1">AOS Period</th><th class="sorting" rowspan="1" colspan="1">ACAD Period</th>';
                     $content .='<th class="sorting" rowspan="1" colspan="1">College</th><th class="sorting" rowspan="1" colspan="1">AOS Description</th><th class="sorting" rowspan="1" colspan="1">Full Description</th><th class="sorting" rowspan="1" colspan="1">School</th><th class="sorting" rowspan="1" colspan="1">AOS Type</th>';
                     
@@ -235,8 +235,8 @@
                     while ($row = $result->fetch_object()) {
                         $content .='<tr class="gradeA odd">';
                         //$content .='<td class="sorting_1"><input type="radio" value="0" id="remove_'.$row->enrolmentid.'" name="remove_'.$row->enrolmentid.'"></td>';
-                        $content .='<td class="sorting_1">'.$row->Type.'</td>';
                         $content .='<td class="sorting_1">'.$row->record_id.'</td>';
+                        $content .='<td class="sorting_1">'.$row->Type.'</td>';
                         $content .='<td class="sorting_1">'.$row->enrolmentid.'</td>';
                         $content .='<td class="sorting_1">'.$row->staffid.'</td>';
                         $content .='<td class="sorting_1">'.$row->stageid.'</td>';
