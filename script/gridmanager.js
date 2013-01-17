cff72c941962e46d543302bf62c90f7dab230632
Daniel Somers <d.somers@ulcc.ac.uk>
2013-01-17 13:32:21 +0000

    testing

--- script/gridmanager.js
+++ script/gridmanager.js
@@ -20,7 +20,7 @@ var gridmanager = gridmanager || (function() {
                    
                    if(confirm("Are you sure you want to remove this enrolment?")==true) {
                         alert("record: " + id +" has been removed.");
-                        $('#table-example').dataTable().fnDeleteRow(selectedRow);
+                        $('#table-example').dataTable().fnDeleteRow(selectedRow, null, true);
                         //$(this).closest('tr').remove();
                         //$('.paginate_active').click();
                    } else {
