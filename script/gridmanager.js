49aacd43f6fff0236111938e452a6baedf904849
Daniel Somers <d.somers@ulcc.ac.uk>
2013-01-17 13:19:43 +0000

    testing

--- script/gridmanager.js
+++ script/gridmanager.js
@@ -10,15 +10,19 @@ var gridmanager = gridmanager || (function() {
         
         $('table tr').click(function() {
            var id=$(this).closest('tr').children('td:first').text();
-           
+           var selectedRow=$(this).closest('tr');
+           var dt = $('#table-example').dataTable();
+                    
            // check if id is a valid number
            if(id*1==id) {
                
                if($('body').attr('id')=="user-enrolments") {
+                   
                    if(confirm("Are you sure you want to remove this enrolment?")==true) {
                         alert("record: " + id +" has been removed.");
-                        $(this).closest('tr').remove();
-                        $('.paginate_active').click();
+                        dt.fnDeleteRow(removeRow);
+                        //$(this).closest('tr').remove();
+                        //$('.paginate_active').click();
                    } else {
                         alert('no action');
                    }
