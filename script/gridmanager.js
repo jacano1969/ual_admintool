997abb90cdd39daf93c04445aa51994db1c4c052
Daniel Somers <d.somers@ulcc.ac.uk>
2013-01-17 12:41:57 +0000

    testing

--- script/gridmanager.js
+++ script/gridmanager.js
@@ -10,7 +10,6 @@ var gridmanager = gridmanager || (function() {
         
         $('table tr').click(function() {
            var id=$(this).closest('tr').children('td:first').text();
-           alert('This Id: ' + id);
            
            // check if id is a valid number
            if(id*1==id) {
